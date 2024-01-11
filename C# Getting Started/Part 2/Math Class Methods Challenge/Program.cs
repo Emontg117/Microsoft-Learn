ce2a665af569a716896029848d6d2525c19ca6e7
Elisha Montgomery <Emontg117@gmail.com>
2024-01-11 06:29:13 -0800

    Math Class Methods Challenge
    https://learn.microsoft.com/en-us/training/modules/csharp-call-methods/5-challenge

---
+++ C# Getting Started/Part 2/Math Class Methods Challenge/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Author: Elisha Montgomery
+Title: Microsoft Learn - Math Class Methods Challenge
+
+Instructions: 
+- Find a method of the System.Math class that returns the larger of two numbers.
+- Research the method and successfully implement it into the prewritten code given.
+- The final solution for this challenge must use the given code, and must use the Console.WriteLine(largerValue); statement to generate the output.
+- The application should output: 600.
+
+Given Code:
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+Console.WriteLine(largerValue);
+
+*/
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue); // Max() is a method of the System.Math class that returns the larger of two given numbers.
+
+Console.WriteLine(largerValue);

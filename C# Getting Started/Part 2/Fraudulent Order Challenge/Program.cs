e6429527b917489e0849378bcbfb3890c1f47dd5
Elisha Montgomery <Emontg117@gmail.com>
2024-01-11 09:14:39 -0800

    Fraudulent Order Challenge
    https://learn.microsoft.com/en-us/training/modules/csharp-arrays/4-challenge

---
+++ C# Getting Started/Part 2/Fraudulent Order Challenge/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Author: Elisha Montgomery
+Title: Fraudulent Order Challenge
+
+Prompt: Your team has found a pattern. Orders that start with the letter "B" encounter fraud at a rate 25 times greater than the normal rate. 
+You write new code that outputs the Order ID of new orders where the Order ID starts with the letter "B". 
+This will be used by the fraud team to investigate further.
+
+Instructions:
+- Declare an array and initialize it with the following elements:
+B123
+C234
+A345
+C15
+B177
+G3003
+C235
+B179
+
+- Create a foreach statement to iterate through each element of the array.
+- Report the Order IDs that start with the letter "B".
+
+*/
+
+string[] orders = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach (string order in orders)
+{
+    if (order.StartsWith("B"))
+    {
+        Console.WriteLine($"Possible Fraudulent Order: {order}");
+    }
+}

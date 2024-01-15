091fd8d7424dd7543b877f9dc7b5ebde600367b3
Elisha Montgomery <Emontg117@gmail.com>
2024-01-15 14:38:48 -0800

    Conditional Operators Challenge
    https://learn.microsoft.com/en-us/training/modules/csharp-evaluate-boolean-expressions/4-challenge-1

---
+++ C# Getting Started/Part 3/Conditional Operators Challenge/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Author: Elisha Montgomery
+Title: Conditional Operators Challenge
+
+Prompt: Write code to display the result of a coin flip using conditional operators
+
+Instructions:
+- Use the Random class to generate a value
+- Based on the value generated, use the conditional operator to display either heads or tails
+- Challenge should be completed using three lines of code
+*/
+Random rnd = new Random();
+string flip = rnd.Next(2) == 1 ? "Heads" : "Tails" ;
+Console.WriteLine(flip);
